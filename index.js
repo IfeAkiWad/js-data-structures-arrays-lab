29b281fc7cdc602f1791bb8c3188917c6334f5ca
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-02-16 19:12:08 -0600

    commit tree 1cd11d916f08014b7b39d587679c9d0debd38873

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+// Write your solution here
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop("Garfield")
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift("Milo")
+}
+
+function appendDriver(name) {
+    let newDrivers = [...drivers, name]
+    return newDrivers
+}
+
+function   prependDriver(name) {
+    let newDrivers = [name, ...drivers]
+    return newDrivers
+}
+
+function removeLastDriver() {
+    let copyOfDrivers = drivers.slice(0, -1)
+    return copyOfDrivers
+}
+
+function  removeFirstDriver() {
+    let copyOfDrivers = drivers.slice(1)
+    return copyOfDrivers
+}
